fb21d7251dd88ffa563c73aaef4ee0b85d1ecfdc
David Lin <David.lin71@gmail.com>
2021-01-21 10:06:31 -0500

    add solution to 989

---
+++ solutions/ruby/989.rb
@@ -0,0 +1,7 @@
+# 989. Add to Array-Form of Integer
+# @param {Integer[]} a
+# @param {Integer} k
+# @return {Integer[]}
+def add_to_array_form(a, k)
+  (a.join('').to_i + k).to_s.split('').map(&:to_i)
+end
